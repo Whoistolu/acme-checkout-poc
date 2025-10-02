b0c44a92bdb5ce20a87bac27d65af6e42416057a
Whoistolu <digittolu25@gmail.com>
2025-10-03 00:11:40 +0100

    Refactor Product to use BigDecimal for precise price storage and avoid float rounding issues

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,11 @@
+require "bigdecimal"
+
 class Product
   attr_reader :code, :name, :price
 
   def initialize(code:, name:, price:)
     @code = code
     @name = name
-    @price = price.to_f
+    @price = BigDecimal(price.to_s)
   end
 end
