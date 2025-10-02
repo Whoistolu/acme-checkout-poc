a29176060a8e118d03ff129ce6be121c0ba7492c
Whoistolu <digittolu25@gmail.com>
2025-10-02 23:47:49 +0100

    Add RSpec tests for Basket class with sample order scenarios

---
+++ spec/basket_spec.rb
@@ -0,0 +1,58 @@
+require "rspec"
+require_relative "../lib/product"
+require_relative "../lib/product_catalogue"
+require_relative "../lib/delivery_rule"
+require_relative "../lib/delivery_calculator"
+require_relative "../lib/basket"
+require_relative "../lib/offers/offer"
+require_relative "../lib/offers/red_widget_offer"
+
+RSpec.describe Basket do
+  let(:products) do
+    [
+      Product.new(code: "R01", name: "Red Widget", price: 32.95),
+      Product.new(code: "G01", name: "Green Widget", price: 24.95),
+      Product.new(code: "B01", name: "Blue Widget", price: 7.95)
+    ]
+  end
+
+  let(:catalogue) { ProductCatalogue.new(products) }
+
+  let(:delivery_rules) do
+    [
+      DeliveryRule.new(threshold: 50, charge: 4.95),
+      DeliveryRule.new(threshold: 90, charge: 2.95)
+    ]
+  end
+
+  let(:delivery_calculator) { DeliveryCalculator.new(delivery_rules) }
+
+  let(:basket) { Basket.new(catalogue: catalogue, delivery_calculator: delivery_calculator, offers: [RedWidgetOffer.new]) }
+
+  it "calculates total for B01, G01" do
+    basket.add("B01")
+    basket.add("G01")
+    expect(basket.total).to eq(37.85)
+  end
+
+  it "calculates total for R01, R01" do
+    basket.add("R01")
+    basket.add("R01")
+    expect(basket.total).to eq(54.37)
+  end
+
+  it "calculates total for R01, G01" do
+    basket.add("R01")
+    basket.add("G01")
+    expect(basket.total).to eq(60.85)
+  end
+
+  it "calculates total for B01, B01, R01, R01, R01" do
+    basket.add("B01")
+    basket.add("B01")
+    basket.add("R01")
+    basket.add("R01")
+    basket.add("R01")
+    expect(basket.total).to eq(98.27)
+  end
+end
